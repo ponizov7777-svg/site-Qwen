ac9411fe3bbac2a7e72353efca4f60a7f34005db
Andrey Ponizov <ponizov7777@gmail.com>
2026-01-22 16:53:46 +0300

    заменил postcss.config.js

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,10 @@
 // postcss.config.js
 module.exports = {
   plugins: {
-    '@tailwindcss/postcss': {},
+    tailwindcss: {},
+    autoprefixer: {},
+    cssnano: {
+      preset: 'default',
+    },
   },
-};
+}
